ede4c02ed6fce6e5ffd4a6fd2a2c96a8c5c4e451
haussbrandt <haussbrandt@gmail.com>
2026-04-19 20:01:23 +0000

    change voice to a generated one

--- anki.py
+++ anki.py
@@ -120,7 +120,7 @@ class Postprocessor:
     @lru_cache(maxsize=20)
     def call_elevenlabs_api(self, sentence_clean):
         response = self.eleven_client.text_to_speech.convert_with_timestamps(
-            voice_id="DowyQ68vDpgFYdWVGjc3", text=sentence_clean
+            voice_id="nhhJXuFAYDroPyaJSlVA", text=sentence_clean
         )
         return response
 
